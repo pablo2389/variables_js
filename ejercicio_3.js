b1136073f62511e575a0b99a63f203613584b44f
pablo2389 <160980965+pablo2389@users.noreply.github.com>
2024-04-14 12:12:54 -0300

    Add files via upload

---
+++ ejercicio_3.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+ 
+ //-------------------------------------------------------------------
+// 3. El siguiente codigo tiene un error, de qué se trata?
+//    proponga una alternativa para que se imprima 1 en consola, 
+//    manteniendo las operaciones de división.
+
+
+let numerador = 25;
+let denominador = 1;
+let resultado = numerador/denominador;
+
+console.log(resultado);//imprime 1
+
